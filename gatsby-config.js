e4151fe5d49921c94249c3125335025c0a2558ca
rack-of-t <tonyleerackley@gmail.com>
2025-02-09 22:33:17 +0000

    Finished Gatsby Tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,19 @@
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
+    title: "Another  & B",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+  ],
 }
